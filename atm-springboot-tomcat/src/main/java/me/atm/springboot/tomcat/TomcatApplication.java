93ffda2a1a9f54258aa52a0b17818e1fd98936b5
altman <altman798393546@gmail.com>
2023-04-01 17:46:15 +0800

    feat:tomcat demo

--- atm-springboot-tomcat/src/main/java/me/atm/springboot/tomcat/TomcatApplication.java
+++ atm-springboot-tomcat/src/main/java/me/atm/springboot/tomcat/TomcatApplication.java
@@ -2,20 +2,10 @@ package me.atm.springboot.tomcat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TomcatApplication {
     public static void main(String[] args) {
         SpringApplication.run(TomcatApplication.class, args);
-    }
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.setPort(8080);
-        return factory;
     }
 }
